e9dea723cc435313e3ce844e6600cd81bb348cdb
miqjection <erdemnaz01@gmail.com>
2023-05-07 09:36:34 +0300

    ToprakScene düzenlemeler

--- Assets/ToprakSceneAsset/Script/PlayerCollision.cs
+++ Assets/ToprakSceneAsset/Script/PlayerCollision.cs
@@ -7,6 +7,7 @@ public class PlayerCollision : MonoBehaviour
     GameObject destroyedTree;
     public GameObject Tree1, Tree2, Tree3, Tree4;
     Vector3 destroyedTransform;
+    public ParticleSystem partical;
     void Start()
     {
         
@@ -57,9 +58,8 @@ public class PlayerCollision : MonoBehaviour
 
         destroyedTree = GameObject.FindWithTag("Tree1");
         Destroy(destroyedTree);
-        //KesmeParticle
+        partical.Play();
 
-        yield return new WaitForSeconds(0.5f);
         Instantiate(Tree2, destroyedTransform, Quaternion.identity);
     }
 
@@ -72,9 +72,8 @@ public class PlayerCollision : MonoBehaviour
 
         destroyedTree = GameObject.FindWithTag("Tree2");
         Destroy(destroyedTree);
-        //KesmeParticle
+        partical.Play();
 
-        yield return new WaitForSeconds(0.5f);
         Instantiate(Tree3, destroyedTransform, Quaternion.identity);
     }
 
@@ -87,9 +86,8 @@ public class PlayerCollision : MonoBehaviour
 
         destroyedTree = GameObject.FindWithTag("Tree3");
         Destroy(destroyedTree);
-        //KesmeParticle
+        partical.Play();
 
-        yield return new WaitForSeconds(0.5f);
         Instantiate(Tree4, destroyedTransform, Quaternion.identity);
     }
 
@@ -101,17 +99,43 @@ public class PlayerCollision : MonoBehaviour
         yield return new WaitForSeconds(0.5f);
         destroyedTree = GameObject.FindWithTag("Tree4");
         Destroy(destroyedTree);
-        //KesmeParticle
+        partical.Play();
     }
 
     void InsTree()
     {
-        float randomx = Random.Range(-12, 12);
-        float randomz = Random.Range(-15.9f, 11);
+        int random = Random.Range(1, 7);
+        Vector3 InsTransform = new Vector3(-10.85f, 6.81f, -2.91f);
+        Vector3 InsTransform2 = new Vector3(9.24f, 6.81f, -2.91f);
+        Vector3 InsTransform3 = new Vector3(4.19f, 6.81f, -11.7f);
+        Vector3 InsTransform4 = new Vector3(-5.53f, 6.81f, -11.7f);
+        Vector3 InsTransform5 = new Vector3(-5.53f, 6.81f, 6.9f);
+        Vector3 InsTransform6 = new Vector3(4.24f, 6.81f, 6.9f);
 
-        Vector3 InsTransform = new Vector3(randomx, 5.26f, randomz);
+        switch (random)
+        {
+            case 1:
+                Instantiate(Tree1, InsTransform, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(Tree1, InsTransform2, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(Tree1, InsTransform3, Quaternion.identity); 
+                break;
+            case 4:
+                Instantiate(Tree1, InsTransform4, Quaternion.identity); 
+                break;
+            case 5:
+                Instantiate(Tree1, InsTransform5, Quaternion.identity); 
+                break;
+            case 6:
+                Instantiate(Tree1, InsTransform6, Quaternion.identity); 
+                break;
 
-        Instantiate(Tree1, InsTransform, Quaternion.identity);
+
+                Instantiate(Tree1, InsTransform, Quaternion.identity);
+                
+        }
     }
-
 }
